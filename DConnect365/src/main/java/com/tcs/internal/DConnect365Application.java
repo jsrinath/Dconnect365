99ab0626381d9a8694d2802ca7e8222ffd0bfddc
Srinath <Srinath@IP-C0A8C2DE.corp.amazonworkspaces.com>
2019-02-11 07:39:02 +0000

    New

---
+++ DConnect365/src/main/java/com/tcs/internal/DConnect365Application.java
@@ -0,0 +1,14 @@
+package com.tcs.internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DConnect365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DConnect365Application.class, args);
+	}
+
+}
+
---
+++ DConnect365/src/main/java/com/tcs/internal/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.tcs.internal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DConnect365Application.class);
+	}
+
+}
+
